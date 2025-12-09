d05c595d24c5b4ff9d8e2b9dd9da93ee6f361979
Rudransh Shrivastava <rudransh.shr@gmail.com>
2025-12-09 14:46:36 +0530

    problem statement wording was terrible

--- 2025/day08/src/main.rs
+++ 2025/day08/src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, fs, io};
+use std::{cmp::Ordering, collections::HashMap, fs, io};
 
 fn main() {
     let input_path;
@@ -32,26 +32,85 @@ fn main() {
         boxes.push(line);
     }
 
-    println!("------------------------------");
-    let list = make_distance_list(&boxes);
+    let distances = make_distance_list(&boxes);
+    let len = boxes.len();
 
-    for item in list {
-        let (a, b, c) = item;
-        println!("{a}: {b}, {c}");
+    let mut counter = 0;
+    let mut union_find = UnionFind::new(len);
+    for d in distances {
+        if counter == pop_count {
+            break;
+        }
+        counter += 1;
+        if union_find.find(d.1) == union_find.find(d.2) {
+            continue;
+        }
+        union_find.union(d.1, d.2);
+    }
+    let map = union_find.unions();
+
+    for (key, val) in map.iter() {
+        print!("\n{key}: ");
+        for v in val {
+            print!("{v} ");
+        }
+    }
+    let mut sizes: Vec<usize> = map.values().map(|v| v.len()).collect();
+    sizes.sort_by(|a, b| b.cmp(a));
+
+    let result = sizes[0] * sizes[1] * sizes[2];
+    println!("\n------------------------------");
+    println!(
+        "Top 3 sizes: {} * {} * {} = {}",
+        sizes[0], sizes[1], sizes[2], result
+    );
+}
+
+struct UnionFind {
+    parent: Vec<usize>,
+}
+
+impl UnionFind {
+    pub fn new(len: usize) -> Self {
+        let mut parent: Vec<usize> = Vec::with_capacity(len);
+        for idx in 0..len {
+            parent.push(idx);
+        }
+        Self { parent }
+    }
+    pub fn union(&mut self, a: usize, b: usize) {
+        let parent_a = self.find(a);
+        let parent_b = self.find(b);
+        if parent_a != parent_b {
+            self.parent[parent_a] = parent_b;
+        }
+    }
+    pub fn find(&mut self, element: usize) -> usize {
+        if self.parent[element] != element {
+            self.parent[element] = self.find(self.parent[element]);
+        }
+        self.parent[element]
+    }
+    pub fn unions(&mut self) -> HashMap<usize, Vec<usize>> {
+        let mut map: HashMap<usize, Vec<usize>> = HashMap::new();
+        for idx in 0..self.parent.len() {
+            let root = self.find(idx);
+            map.entry(root).or_insert(Vec::new()).push(idx)
+        }
+        map
     }
 }
 
-fn make_distance_list<'a>(boxes: &Vec<&'a str>) -> Vec<(f32, &'a str, &'a str)> {
+fn make_distance_list(boxes: &Vec<&str>) -> Vec<(f32, usize, usize)> {
     let len = boxes.len();
 
-    let mut distances: Vec<(f32, &str, &str)> = Vec::new();
+    let mut distances: Vec<(f32, usize, usize)> = Vec::new();
     for col in 0..len {
-        for row in 0..len {
-            distances.push((
-                calculate_distance(boxes[col], boxes[row]),
-                boxes[col],
-                boxes[row],
-            ));
+        for row in col + 1..len {
+            if boxes[col] == boxes[row] {
+                continue;
+            }
+            distances.push((calculate_distance(boxes[col], boxes[row]), col, row));
         }
     }
     distances.sort_by(|a, b| match (a.0).total_cmp(&b.0) {
@@ -59,12 +118,19 @@ fn make_distance_list<'a>(boxes: &Vec<&'a str>) -> Vec<(f32, &'a str, &'a str)> 
         Ordering::Greater => Ordering::Greater,
         Ordering::Equal => Ordering::Equal,
     });
+    for (idx, d) in distances.clone().iter().enumerate() {
+        if idx < 30 {
+            println!("a: {} | b: {} | distance: {}", d.1, d.2, d.0);
+        } else {
+            break;
+        };
+    }
     return distances;
 }
 
 fn calculate_distance(a: &str, b: &str) -> f32 {
-    let a: Vec<i32> = a.trim().split(",").map(|c| c.parse().unwrap()).collect();
-    let b: Vec<i32> = b.trim().split(",").map(|c| c.parse().unwrap()).collect();
+    let a: Vec<i64> = a.trim().split(",").map(|c| c.parse().unwrap()).collect();
+    let b: Vec<i64> = b.trim().split(",").map(|c| c.parse().unwrap()).collect();
     let (ax, ay, az) = (a[0], a[1], a[2]);
     let (bx, by, bz) = (b[0], b[1], b[2]);
 
